031147c5ddd0d8094a09ec4b71a953f28b767db3
Mesquita <mesquita@aospa.co>
2021-01-26 21:53:31 -0300

    lmi: Drop unecessary drm configs
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -108,12 +108,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    drm.service.enabled=true
 
 # Fastboot
 PRODUCT_PACKAGES += \
